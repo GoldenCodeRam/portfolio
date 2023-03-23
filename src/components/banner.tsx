f4c5a6f496af6716225e9f70a119d31dfab99af0
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-03-23 00:09:08 -0500

    feat: simple banner and header

---
+++ src/components/banner.tsx
@@ -0,0 +1,10 @@
+export default function Banner() {
+    return (
+        <div className="h-full flex-auto bg-blue-900">
+            <div className="flex flex-col h-full justify-center items-center">
+                <h1 className="text-center text-9xl text-white">Golden Ram Tech</h1>
+                <h2 className="text-7xl text-white">Full-stack Developer</h2>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className="shadow py-3">
+            <div className="container mx-auto">
+                @GoldenRamTech
+            </div>
+        </div>
+    );
+}
