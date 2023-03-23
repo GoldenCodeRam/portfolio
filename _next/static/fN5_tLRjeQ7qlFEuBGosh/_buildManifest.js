7f47a1f201d472cf890826a7c04636109902002c
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-03-23 00:33:08 -0500

    Updates

---
+++ _next/static/fN5_tLRjeQ7qlFEuBGosh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-dbe95e9c62f87c26.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
